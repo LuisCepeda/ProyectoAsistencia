640b53ddbba3cb5877f9e83d4d38492c4d4d480f
LuisCepeda <73725496+LuisCepeda@users.noreply.github.com>
2022-05-26 20:49:43 -0500

    Add files via upload

---
+++ ProyectoAsistencia/ProyectoAsistencia/Form1.vb
@@ -0,0 +1,65 @@
+﻿
+Imports System.Net.Mail
+Imports System.Net
+Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CRUD.strstrconnection()
+        Me.Text = "Email Sending App"
+        Try 'CALL THE METHOD THAT YOU HAVE CREATED. 
+            'PUT YOUR QUERY AND THE NAME OF THE DATAGRIDVIEW IN THE PARAMETERS. 
+            'THIS IS METHOD IS FOR RETREIVING THE DATA IN THE DATABASE TO THE DATAGRIDVIEW reload("SELECT * FROM users", DTGLIST) 
+            Dim query As String
+            query = "SELECT e.primerApellido,e.segundoApellido,e.primerNombre,e.segundoNombre
+                     FROM estudiante e"
+            CRUD.reload(query, DataGridView1)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Try 'CALL THE METHOD THAT YOU HAVE CREATED. 
+            'PUT YOUR QUERY AND THE NAME OF THE DATAGRIDVIEW IN THE PARAMETERS. 
+            'THIS IS METHOD IS FOR RETREIVING THE DATA IN THE DATABASE TO THE DATAGRIDVIEW reload("SELECT * FROM users", DTGLIST) 
+            Dim query As String
+            query = "SELECT e.primerApellido,e.segundoApellido,e.primerNombre,e.segundoNombre
+                     FROM estudiante e"
+            CRUD.reload(query, DataGridView1)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim plantilla As String
+        plantilla = "los siguientes"
+        Try
+            Dim Smtp_Server As New SmtpClient
+            Dim e_mail As New MailMessage()
+            Smtp_Server.UseDefaultCredentials = False
+            Smtp_Server.Credentials = New Net.NetworkCredential("pruebaproyectobasededatos@gmail.com", "proyectoAsistencia")
+            Smtp_Server.Port = 587
+            Smtp_Server.EnableSsl = True
+            Smtp_Server.Host = "smtp.gmail.com"
+            Smtp_Server.DeliveryMethod = SmtpDeliveryMethod.Network
+
+            e_mail = New MailMessage()
+            e_mail.From = New MailAddress("pruebaproyectobasededatos@gmail.com")
+            e_mail.To.Add("luissebastian.cepedaflechas@gmail.com")
+            e_mail.Subject = "Prueba correo"
+            e_mail.IsBodyHtml = False
+            e_mail.Body = plantilla
+            Smtp_Server.Send(e_mail)
+            MsgBox("Mail Sent")
+
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+    End Sub
+End Class
---
+++ ProyectoAsistencia/ProyectoAsistencia/Modules/CRUD.vb
@@ -0,0 +1,116 @@
+﻿Imports MySql.Data.MySqlClient
+Module CRUD
+    'SETTING UP THE CONNECTION
+
+    Public Function strstrconnection() As MySqlConnection
+        Dim vConnString As New MySqlConnectionStringBuilder
+        'vConnString.Server = "localhost"
+        'vConnString.Port = "3308"
+        'vConnString.Database = "proyectoasistencia"
+        'vConnString.UserID = "root"
+        'vConnString.Password = "pass123SQL"
+
+        vConnString.Server = "bwv4kwv0evdypdwgjby1-mysql.services.clever-cloud.com"
+        vConnString.Port = "3306"
+        vConnString.Database = "bwv4kwv0evdypdwgjby1"
+        vConnString.UserID = "u75eetfmdauejadh"
+        vConnString.Password = "Ijyi2awemy70KNv33P1M"
+
+        vConnString.IntegratedSecurity = False
+        Return New MySqlConnection(vConnString.ToString)
+
+    End Function
+
+    Public strcon As MySqlConnection = strstrconnection()
+    'DECLARING CLASSES AND VARIABLE
+    Public result As String
+    Public cmd As New MySqlCommand
+    Public da As New MySqlDataAdapter
+    Public dt As New DataTable
+
+    'THIS METHOD IS FOR INSERTING DATA IN THE DATABASE
+    Public Sub create(ByVal sql As String)
+        Try
+            strcon.Open()
+            'HOLDS THE DATA TO BE EXECUTED
+            With cmd
+                .Connection = strcon
+                .CommandText = sql
+                'EXECUTE THE DATA
+                result = cmd.ExecuteNonQuery
+                'CHECKING IF THE DATA HAS EXECUTED OR NOT AND THEN THE POP UP MESSAGE WILL APPEAR
+                If result = 0 Then
+                    MsgBox("FAILED TO SAVE THE DATA", MsgBoxStyle.Information)
+                Else
+                    MsgBox("DATA HAS BEEN SAVE IN THE DATABASE")
+                End If
+            End With
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        strcon.Close()
+    End Sub
+
+
+    'THIS METHOD IS FOR RETRIEVING DATA IN THE DATABASE
+    Public Sub reload(ByVal sql As String, ByVal DTG As Object)
+        Try
+            dt = New DataTable
+            strcon.Open()
+            With cmd
+                .Connection = strcon
+                .CommandText = sql
+            End With
+            da.SelectCommand = cmd
+            da.Fill(dt)
+            DTG.DataSource = dt
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        strcon.Close()
+        da.Dispose()
+    End Sub
+
+    'THIS METHOD IS FOR UPDATING THE DATA IN THE DATABASE.
+    Public Sub updates(ByVal sql As String)
+        Try
+            strcon.Open()
+            With cmd
+                .Connection = strcon
+                .CommandText = sql
+                result = cmd.ExecuteNonQuery
+                If result = 0 Then
+                    MsgBox("DATA IS FAILED TO UPDATE.", MsgBoxStyle.Information)
+                Else
+                    MsgBox("THE DATA HAS BEEN UPDATED IN THE DATABASE.")
+                End If
+            End With
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        strcon.Close()
+    End Sub
+
+    'THIS METHOD IS FOR DELETING THE DATA IN THE DATABASE
+    Public Sub delete(ByVal sql As String)
+        Try
+            strcon.Open()
+            With cmd
+                .Connection = strcon
+                .CommandText = sql
+                result = cmd.ExecuteNonQuery
+                If result = 0 Then
+                    MsgBox("FAILED TO DELETE THE DATA IN THE DATABASE.", MsgBoxStyle.Critical)
+                Else
+                    MsgBox("DATA HAS BEEN DELETED IN THE DATABASE.")
+                End If
+            End With
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        strcon.Close()
+    End Sub
+
+
+End Module
